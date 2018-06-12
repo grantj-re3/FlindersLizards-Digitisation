740722faedb9bb7c9064616b43b8645a33199d5f
grantj-re3 <grantj-re3@github.fake>
2018-06-12 15:07:14 +0930

    Allow the PDF image resolution to to be within some limit (MAX_ERROR_PERCENT).

--- bin/check_res.sh
+++ bin/check_res.sh
@@ -22,6 +22,40 @@ use_quote=$true		# If true then wrap every field in double quotes.
 
 rootname_img="img1"
 fname_img="$rootname_img-000.ppm"
+
+##############################################################################
+get_res_status() {
+  info="$1"
+
+  echo "$info" |grep -q -P " size = \d+ x \d+$" && {
+    status=`
+      echo "$info" |
+        sed 's!^.* size = !!; s! x ! !' |
+        ruby -ane '
+          MAX_ERROR_PERCENT = 5
+          NOM_RES = [7016, 4961]		# Nominal resolution
+
+          def isWithinPct(val, nom_val)
+            return nil if nom_val.to_i == 0	# Invalid resolution
+            ((val.to_f / nom_val.to_i - 1) * 100).abs <= MAX_ERROR_PERCENT
+          end
+
+          # Main
+          if isWithinPct($F[0], NOM_RES[0]) && isWithinPct($F[1], NOM_RES[1])
+            puts "-"
+          else
+            puts "BadRes"
+          end
+        '
+    `
+  } || {
+    status="ResInfoNotFound"
+  }
+}
+
+##############################################################################
+# Main
+##############################################################################
 [ -f  "$fname_img" ] && rm -f "$fname_img"
 
 printf "$fmt" "Filename" $delim "Status" $delim "ImageInfo" 	# CSV header line
@@ -30,8 +64,7 @@ for f in ../src/slls_*.pdf; do
   info=`pdfimages -f 1 -l 1 $f "$rootname_img" && file "$fname_img"`
   [ -f  "$fname_img" ] && rm -f "$fname_img"
 
-  status="-"
-  if ! echo "$info" |egrep -q " size = 7016 x 4961$"; then status="BadRes"; fi
+  get_res_status "$info"
 
   # RFC 4180 says a double quote "must be escaped by preceding it with another double quote"
   [ "$use_quote" ] && csv_info=`echo "$info" |sed 's!"!""!g'` || csv_info="$info"
